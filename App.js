ddb789f5778378c9410c366f9acc81d9d59cc4f5
NkAndre <andre270708@gmail.com>
2026-04-27 14:46:52 -0300

    refactor: remove UI da branch de lógica

--- App.js
+++ App.js
@@ -1,51 +1,11 @@
-import { View, Text, Button, StyleSheet } from "react-native";
-import { useState } from "react";
-import { girar, getAlerta, reset } from "./src/logic/game";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [saldo, setSaldo] = useState(100);
-  const [msg, setMsg] = useState("");
-  const [alerta, setAlerta] = useState("");
-
-  const handleGirar = () => {
-    const r = girar();
-
-    setSaldo(r.saldo);
-    setMsg(r.resultado === "ganhou" ? "Você ganhou!" : "Você perdeu!");
-
-    const a = getAlerta();
-    if (a) setAlerta(a);
-  };
-
-  const handleReset = () => {
-    reset();
-    setSaldo(100);
-    setMsg("");
-    setAlerta("");
-  };
-
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Simulador de Apostas</Text>
-
-      <Text style={styles.saldo}>Saldo: R$ {saldo}</Text>
-
-      <Button title="Girar" onPress={handleGirar} />
-
-      <Text
-        style={[
-          styles.resultado,
-          msg.includes("ganhou") ? styles.ganhou : styles.perdeu
-        ]}
-      >
-        {msg}
-      </Text>
-
-      {alerta !== "" && (
-        <Text style={styles.alerta}>{alerta}</Text>
-      )}
-
-      <Button title="Resetar" onPress={handleReset} />
+      <Text>Hello world</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -53,32 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  title: {
-    fontSize: 22,
-    marginBottom: 20,
-    fontWeight: "bold"
-  },
-  saldo: {
-    fontSize: 18,
-    marginBottom: 15
-  },
-  resultado: {
-    marginTop: 15,
-    fontSize: 18
-  },
-  ganhou: {
-    color: "green"
-  },
-  perdeu: {
-    color: "red"
-  },
-  alerta: {
-    marginTop: 20,
-    color: "red",
-    textAlign: "center"
-  }
 });
