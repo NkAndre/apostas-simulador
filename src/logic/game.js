7e59cdc5acf6d11b9b2d1203eac00d3579fb400a
NkAndre <andre270708@gmail.com>
2026-04-29 14:29:11 -0300

    feat logica pronta

--- src/logic/game.js
+++ src/logic/game.js
@@ -1,21 +1,34 @@
-let saldo = 100;
+let saldo = 100; // define o saldo inicial do jogador 
+
+// aq o contador para registrar quantas vezes o botão foi clicado
 let jogadas = 0;
+
+// criei um arraay para listar os detalhes de cada rodada 
 let historico = [];
 
+
+
 function girar() {
+
+  //incrementar o contador de jogadqas toda vez que a funçao é ativada
   jogadas++;
 
   let ganhou;
 
-  if (jogadas <= 2) {
-    ganhou = true; // ilusão inicial
-  } else {
-    ganhou = Math.random() < 0.3;
-  }
 
+  // aqui comeca a manipulaçao do APP
+
+  // isca/; se forem as duas primeiras jogadas iniciais, o jogador sempre vai vencer(ilusao)
+  if (jogadas <= 2) {
+    ganhou = true;
+  } else {
+    ganhou = Math.random() < 0.3; //  a partir da 3 rodada , a chance de vencer é de apns 30 %
+  }
+  //// se ganhou, recebe 20. se perdeu, retira 10 do saldo
   let valor = ganhou ? 20 : -10;
   saldo += valor;
 
+  // cria um objeto com os dados desta rodada 
   const resultado = {
     jogada: jogadas,
     resultado: ganhou ? "ganhou" : "perdeu",
@@ -23,26 +36,43 @@ function girar() {
     saldo
   };
 
+
+  // salva o resultado no array de histórico
   historico.push(resultado);
+
+  // retorna os dados da rodada para quem chamou a função
   return resultado;
 }
 
-function getAlerta() {
+ // msg de concientização 
+function getAlerta(ganhouAgora) {
+
+  if(ganhouAgora){
+    return null;
+  }
+
+  // se o jogador insistiu (5+ vezes) e já esta no prejuízo
   if (jogadas >= 5 && saldo < 100) {
     return "Você está perdendo dinheiro. Esses jogos são feitos pra te prender.";
   }
 
+  // se o jogador chegou a 10 jogadas, envia um alerta sobre o padrão de perda
   if (jogadas >= 10) {
     return "Percebeu o padrão? Quanto mais joga, mais perde.";
   }
 
+  // se nao houver alertas retorna nada 
   return null;
 }
 
+// reinicia todas as variáveis para o estado original (limpa o jogo) famosin buffer
 function reset() {
   saldo = 100;
   jogadas = 0;
   historico = [];
 }
 
+// exporta as funções para serem usadas em outros arquivos do projeto
 export { girar, getAlerta, reset };
+
+
